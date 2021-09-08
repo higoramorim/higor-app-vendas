1e4fdfc216d6216e48db5040c4554c20dfbc3ada
higoramorim <1810042@aluno.univesp.br>
2021-09-08 10:15:48 -0300

    Layers

---
+++ backend/dsvendas/src/main/java/com/higor/app/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.higor.app.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.higor.app.dsvendas.dto.SellerDTO;
+import com.higor.app.dsvendas.entities.Seller;
+import com.higor.app.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
